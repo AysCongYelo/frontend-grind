84dc09b601907589c08ae74f39246326cd3cfdda
AysCongYelo <pascual.eduardace@gmail.com>
2026-06-22 13:16:16 +0800

    Finish DOM selecting elements

---
+++ module-04-dom/01-selecting-elements/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// ====================
+// GET ELEMENT BY ID
+// ====================
+
+const title = document.getElementById("title");
+
+console.log(title);
+
+// ====================
+// GET ELEMENTS BY CLASS
+// ====================
+
+const texts = document.getElementsByClassName("text");
+
+console.log(texts);
+
+// ====================
+// QUERY SELECTOR
+// ====================
+
+const firstText = document.querySelector(".text");
+
+console.log(firstText);
+
+// ====================
+// QUERY SELECTOR ALL
+// ====================
+
+const allTexts = document.querySelectorAll(".text");
+
+console.log(allTexts);
