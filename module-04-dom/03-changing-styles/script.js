9d10bc8bf54bc082887e63b8c4f532134e645c99
AysCongYelo <pascual.eduardace@gmail.com>
2026-06-22 13:28:06 +0800

    Finish DOM changing styles

---
+++ module-04-dom/03-changing-styles/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// ====================
+// CHANGE TEXT COLOR
+// ====================
+
+const title = document.getElementById("title");
+
+title.style.color = "red";
+
+// ====================
+// CHANGE BACKGROUND COLOR
+// ====================
+
+title.style.backgroundColor = "yellow";
+
+// ====================
+// CHANGE FONT SIZE
+// ====================
+
+title.style.fontSize = "40px";
+
+// ====================
+// CHANGE FONT WEIGHT
+// ====================
+
+title.style.fontWeight = "700";
+
+// ====================
+// CHANGE TEXT ALIGNMENT
+// ====================
+
+title.style.textAlign = "center";
