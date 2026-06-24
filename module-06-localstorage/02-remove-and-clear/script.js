f5233e0273da3b61813e90fd8f71b6b93303632e
AysCongYelo <pascual.eduardace@gmail.com>
2026-06-24 10:49:05 +0800

    Finish LocalStorage remove and clear

---
+++ module-06-localstorage/02-remove-and-clear/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// ====================
+// SAVE DATA
+// ====================
+
+localStorage.setItem("username", "Ace");
+localStorage.setItem("theme", "dark");
+
+// ====================
+// REMOVE ONE ITEM
+// ====================
+
+localStorage.removeItem("theme");
+
+// ====================
+// CLEAR ALL ITEMS
+// ====================
+
+// localStorage.clear();
