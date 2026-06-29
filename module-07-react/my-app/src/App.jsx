a3c4031836b7f9e60f08316d7fcf44ed1243eb19
AysCongYelo <pascual.eduardace@gmail.com>
2026-06-29 10:52:58 +0800

    Implement React conditional rendering

--- module-07-react/my-app/src/App.jsx
+++ module-07-react/my-app/src/App.jsx
@@ -1,15 +1,19 @@
-import { useEffect } from "react";
-
 function App() {
-  useEffect(() => {
-    async function fetchUsers() {
-      console.log("Fetching users...");
-    }
+  const status = "";
 
-    fetchUsers();
-  }, []);
+  if (status === "loading") {
+    return <h1>Loading...</h1>;
+  }
 
-  return <h1>Fetch Example</h1>;
+  if (status === "success") {
+    return <h1>Data Loaded!</h1>;
+  }
+
+  if (status === "error") {
+    return <h1>Something went wrong.</h1>;
+  }
+
+  return <h1>Unknown Status</h1>;
 }
 
 export default App;
