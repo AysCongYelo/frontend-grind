c5f7117f1aef71521f7db0e4932d580ada0fe827
AysCongYelo <pascual.eduardace@gmail.com>
2026-06-29 10:05:37 +0800

    Complete React useEffect hook

--- module-07-react/my-app/src/App.jsx
+++ module-07-react/my-app/src/App.jsx
@@ -2,19 +2,14 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    console.log("Component mounted");
+    async function fetchUsers() {
+      console.log("Fetching users...");
+    }
 
-    const timer = setInterval(() => {
-      console.log("Tick");
-    }, 1000);
-
-    return () => {
-      clearInterval(timer);
-      console.log("Cleanup");
-    };
+    fetchUsers();
   }, []);
 
-  return <h1>Cleanup Example</h1>;
+  return <h1>Fetch Example</h1>;
 }
 
 export default App;
