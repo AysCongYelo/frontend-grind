52b56a4dd97c0a636330bdbd64eacf1cc87c5168
AysCongYelo <pascual.eduardace@gmail.com>
2026-06-30 09:35:50 +0800

    feat: add counter state and display

--- projects/counter-app/src/App.jsx
+++ projects/counter-app/src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
+
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>{count}</h1>
     </>
   );
 }
